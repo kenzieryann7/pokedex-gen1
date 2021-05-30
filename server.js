83617ed1c490113327f89a5ba4b6dbfa906be33f
kenzieryann7 <makenzie_99@hotmail.com>
2021-05-29 21:11:40 -0700

    got server running through html

--- server.js
+++ server.js
@@ -1,48 +1,14 @@
-var htmlFile = fs.readFileSync('public/index.html');
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 
-console.log("PORT:", process.env.PORT);
+app.use(express.static('public'));
 
-function requestHandler(req, res) {
-    console.log("Method:", req.method);
-    console.log("URL:", req.url);
-    console.log("Headers:", req.headers);
+app.get('*', function (req, res) {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
   
-    if ((req.url == '/index.html') || (req.url == '/')) {
-      res.writeHead(200, {
-        "Content-Type": "text/html"
-      });
-      res.write(htmlFile);
-    }
-    else if (req.url == '/style.css') {
-      res.writeHead(200, {
-        "Content-Type": "text/css"
-      });
-      res.write(cssFile);
-    }
-    else if (req.url == '/index.js') {
-      res.writeHead(200, {
-        "Content-Type": "text/js"
-      });
-      res.write(jsFile);
-    }
-    else if (req.url == '/404.html') {
-      res.writeHead(200, {
-        "Content-Type": "text/html"
-      });
-      res.write(errorFile);
-    }
-    else {
-      res.writeHead(404, {
-        "Content-Type": "text/html"
-      });
-      res.write(errorFile);
-    }
-    res.end();
-  }
-  
-  var server = http.createServer(requestHandler);
-  
-  server.listen(3000, function (err) {
-    console.log("Server is running on port 3000");
-    console.log("Go to http://localhost:3000/");
-  });
+app.listen(port, function () {
+    console.log("== Server is listening on port", port);
+});
